a1befa533a3bd2b2bddbee6c3226c6e2bc0f2ab0
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2025-07-13 13:40:47 +0100

    Add displays table to kohastructure.sql

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -2880,6 +2880,33 @@ CREATE TABLE `discharges` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `displays`
+--
+
+DROP TABLE IF EXISTS `displays`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `displays` (
+  `display_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique id for the display',
+  `display_name` varchar(255) DEFAULT NULL COMMENT 'the name of the display',
+  `start_date` date DEFAULT NULL COMMENT 'the start date of the display (optional)',
+  `end_date` date DEFAULT NULL COMMENT 'the end date of the display (optional)',
+  `display_days` int(11) DEFAULT NULL COMMENT 'the number of days an item should stay in the display (optional)',
+  `enabled` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'determines whether the display is active',
+  `display_location` varchar(80) DEFAULT NULL COMMENT 'the shelving location for the display (optional)',
+  `display_code` varchar(80) DEFAULT NULL COMMENT 'the collection code for the display (optional)',
+  `display_branch` varchar(10) DEFAULT NULL COMMENT 'a new home branch for the item to have while on display (optional)',
+  `display_type` varchar(10) DEFAULT NULL COMMENT 'a new itype for the item to have while on display (optional)',
+  `display_return_over` enum('yes','any library','not home library','no') NOT NULL DEFAULT 'no' COMMENT 'should the item be removed from the display when it is returned',
+  PRIMARY KEY (`display_id`),
+  KEY `display_branch` (`display_branch`),
+  KEY `display_type` (`display_type`),
+  CONSTRAINT `displays_ibfk_1` FOREIGN KEY (`display_branch`) REFERENCES `branches` (`branchcode`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `displays_ibfk_2` FOREIGN KEY (`display_type`) REFERENCES `itemtypes` (`itemtype`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `edifact_ean`
 --
 
