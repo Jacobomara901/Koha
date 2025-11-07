7bf1a907de025ce264730a6634c371a0100b3129
Lucas Gass <lucas@bywatersolutions.com>
2025-11-07 20:19:12 +0000

    Bug 34069: Move permission from borrower to tools
    Signed-off-by: Katie Bliss <kebliss@dmpl.org>

--- tools/restore_deleted_borrowers.pl
+++ tools/restore_deleted_borrowers.pl
@@ -39,7 +39,7 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         template_name => "tools/restore_deleted_borrowers.tt",
         query         => $input,
         type          => "intranet",
-        flagsrequired => { borrowers => 'restore_deleted_borrowers' },
+        flagsrequired => { tools => 'restore_deleted_borrowers' },
     }
 );
 
